2056ccb11a6b9257cf011d763f2a53f9b6304a3c
mattiacoli <mattia.coli@aol.com>
2025-02-18 15:31:38 +0100

    add event listener and function to turn on lamp

---
+++ main.js
@@ -0,0 +1,9 @@
+const btnEL = document.querySelector('button')
+const lampEL = document.querySelector('img')
+
+btnEL.addEventListener('click', function (){
+  
+lampEL.src = './assets/img/yellow_lamp.png'
+
+  
+})
